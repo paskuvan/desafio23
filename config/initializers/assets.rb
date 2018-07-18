329a41e32b5ad9679df671abcd8344cffc35a265
paskuvan <majopaskuvan@gmail.com>
2018-07-17 22:48:53 -0400

    upload js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'images')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
